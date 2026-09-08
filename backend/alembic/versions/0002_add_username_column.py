1a8a2e271637bd8ded93662ab390084a27d19458
BARATH RAJ V <velusamybarathraj@gmail.com>
2026-09-09 00:19:08 +0530

    fix: correct migration revision IDs

--- backend/alembic/versions/0002_add_username_column.py
+++ backend/alembic/versions/0002_add_username_column.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers
-revision = '0002'
-down_revision = '0001'
+revision = '0002_add_username'
+down_revision = '0001_initial'
 branch_labels = None
 depends_on = None
 
